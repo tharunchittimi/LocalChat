02a176e900c6e1e75c4cbe491b2176504ce9f4d3
TharunKumarC <tharunkumarchittimi@gmail.com>
2021-08-09 12:19:12 +0530

    Removed unused classes.

--- app/src/main/java/com/example/localchat/base/room/dao/RoomDao.kt
+++ app/src/main/java/com/example/localchat/base/room/dao/RoomDao.kt
@@ -11,16 +11,10 @@ interface RoomDao {
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     fun insertGroup(roomEntity: RoomEntity): Long
 
-//    @Query("SELECT LAST_INSERT_ID() from GroupEntity")
-//    fun getLastGroupId():Long
-
     @Query("SELECT * from RoomEntity where groupId = :groupId limit 1")
     fun getInsertedGroupData(groupId:Long?): RoomEntity
 
-
     @Query("select RE.groupId from RoomUsersEntity as RUE join RoomEntity as RE on RUE.mGroupId = RE.groupId join  (select RE1.groupId from RoomUsersEntity as RUE1  join RoomEntity as RE1 on RUE1.mGroupId = RE1.groupId where RE1.isGroup = 0 and RUE1.userIds = :currentUserId) as Q1 on Q1.groupId = RE.groupId where RUE.userIds = :toUserId  and RE.isGroup = 0")
     fun getGroupId(currentUserId:String?,toUserId:String?):Long?
-
-
 
 }
--- app/src/main/java/com/example/localchat/base/room/dao/RoomUserDao.kt
+++ app/src/main/java/com/example/localchat/base/room/dao/RoomUserDao.kt
@@ -6,17 +6,8 @@ import com.example.localchat.base.room.entities.RoomUsersEntity
 @Dao
 interface RoomUserDao {
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    fun insertGroupUser(roomUsersEntity: RoomUsersEntity): Long
-
-
-    @Insert(onConflict = OnConflictStrategy.REPLACE)
     fun insertGroupUsers(roomUsersEntity: List<RoomUsersEntity>)
 
-//    @Transaction
-//    @Query("SELECT * FROM GroupEntity")
-//    fun getUsersWithGroup(): List<GroupUsersEntity>
-//
-//
     @Query("select UE.userName from ROOMUSERSENTITY RUE left join userEntity UE on RUE.userIds = UE.userId where RUE.mGroupId = :groupId and RUE.userIds != :currentUserId ")
     fun getGroupMembers(groupId:Long?, currentUserId:String?): List<String?>
 }
